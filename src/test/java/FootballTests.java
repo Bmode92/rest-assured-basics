90e54c315e61b0fb76dc42dc233e2c4ba6b0a0d9
Ioan <sioan@luxoft.com>
2025-02-11 14:35:29 +0200

    extract data from restassured

--- src/test/java/FootballTests.java
+++ src/test/java/FootballTests.java
@@ -1,8 +1,12 @@
 import config.FootballConfig;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class FootballTests extends FootballConfig {
 
@@ -22,5 +26,77 @@ public class FootballTests extends FootballConfig {
                 .queryParam("areas", areaIds);
         when()
                 .get("/areas");
+    }
+
+    @Test
+    public void getDateFounded() {
+        given()
+                .when()
+                .get("teams/57")
+                .then()
+                .body("founded", equalTo(1886));
+    }
+
+    @Test
+    public void getFirstTeamName() {
+        given()
+                .when()
+                .get("competitions/2021/teams")
+                .then()
+                .body("teams.name[0]", equalTo("Arsenal FC"));
+    }
+
+    @Test
+    public void getAllTeamData() {
+        String responseBody = get("teams/57").asString();
+        System.out.println(responseBody);
+    }
+
+    @Test
+    public void getAllTeamData_DoCheckFirst() {
+        Response response =
+                given()
+                        .when()
+                        .get("teams/57")
+                        .then()
+                        .contentType(ContentType.JSON)
+                        .extract().response();
+
+        String jsonResponseAsString = response.asString();
+        System.out.println(jsonResponseAsString);
+    }
+
+    @Test
+    public void extractHeaders() {
+        Response response =
+                get("teams/57")
+                        .then()
+                        .extract().response();
+
+        String contentTypeHeader = response.getContentType();
+        System.out.println(contentTypeHeader);
+
+        String apiVersionHeader = response.header("X-API-Version");
+        System.out.println(apiVersionHeader);
+    }
+
+    @Test
+    public void extractFirstTeamName() {
+        String firstTeamName = get("competitions/2021/teams")
+                .jsonPath()
+                .getString("teams.name[0]");
+        System.out.println(firstTeamName);
+    }
+
+    @Test
+    public void extractAllTeamNames() {
+        Response response = get("competitions/2021/teams")
+                .then()
+                .extract().response();
+
+        List<String> teamNames = response.path("teams.name");
+        for (String teamName : teamNames) {
+            System.out.println(teamName);
+        }
     }
 }
